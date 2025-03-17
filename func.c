7ae36e1229cb2843056f90943c47077f7b1e2f96
Makar Molochaev <Makara1006@gmail.com>
2025-03-17 22:17:51 +0300

    славик лох

--- func.c
+++ func.c
@@ -11,6 +11,12 @@ int Func(const char* filename, const char* output)
     int col_capacity = 8;
     int maxLength = 0;
     int* toDelete;
+    int toDeleteCount = 0;
+    int delCounter = 0;
+    int flag = 0;
+    int C = 0;
+    int flag1 = 0;
+    int* newLengths;
     Matrix m;
 
     m.count = 0;
@@ -54,11 +60,17 @@ int Func(const char* filename, const char* output)
 
     fclose(file);
 
-    toDelete = (in)
 
     for(int i=0; i<m.count; i++)
     {
         maxLength = (m.lens[i] > maxLength) ? m.lens[i] : maxLength;
+    }
+
+    toDelete = (int*)malloc(maxLength * sizeof(int));
+    newLengths = (int*)malloc(m.count * sizeof(int));
+
+    for(int i=0;i<m.count;i++){
+        newLengths[i] = m.lens[i];
     }
 
     for(int i=0;i<maxLength;i++)
@@ -71,10 +83,10 @@ int Func(const char* filename, const char* output)
                 continue;
             }
             sum += m.els[j][i];
-            printf("%d ", m.els[j][i]);
+            //printf("%d ", m.els[j][i]);
             countInColumn++;
         }
-        printf("\n");
+        //printf("\n\n");
         for(int j=0;j<m.count;j++){
             if(m.lens[j] <= i) {
                 continue;
@@ -82,13 +94,55 @@ int Func(const char* filename, const char* output)
 
             if(m.els[j][i] == 0){
                 if(sum == 0){
-                    printf("%d in row %d\n", m.els[j][i], i);
+                    toDelete[toDeleteCount] = i;
+                    toDeleteCount++;
+                    break;
+                    //printf("%d in row %d\n", m.els[j][i], i);
                 }
             }
             else if(sum % m.els[j][i] == 0) {
-               printf("%d in row %d\n", m.els[j][i], i); 
+                toDelete[toDeleteCount] = i;
+                toDeleteCount++;
+                break;
+                //printf("%d in row %d\n", m.els[j][i], i);
             }
         }
+    }
+
+    for(int i=0;i<toDeleteCount;i++){
+        printf("%d ", toDelete[i]);
+    }
+    printf("\n");
+
+    while(flag < maxLength){
+        if(toDelete[delCounter] == flag)
+        {
+            flag1 = 1;
+            delCounter++;
+            flag++;
+            continue;
+        }
+        printf("%d -> %d\n", flag, C);
+        for(int j=0;j<m.count;j++){
+            if(m.lens[j] <= C) {
+                continue;
+            }
+            if(flag1 == 1){
+                newLengths[j]--;
+            }
+            m.els[j][C] = m.els[j][flag];
+        }
+        flag1=0;
+        C++;
+        flag++;
+    }
+
+    // Вывод данных для проверки
+    for (int i = 0; i < m.count; i++) {
+        for (int j = 0; j < newLengths[i]; j++) {
+            printf("%d ", m.els[i][j]);
+        }
+        printf("\n");
     }
 
 
